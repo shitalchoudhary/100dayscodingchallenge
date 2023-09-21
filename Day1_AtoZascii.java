c35301cecfff9c6d7e67a79efd00dbd98149d735
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-09-21 20:24:41 +0530

    Add files via upload
    AtoZascii.java contains code of taking input from command line and printing that input.

---
+++ Day1_AtoZascii.java
@@ -0,0 +1,32 @@
+class AsciiValue{
+	public static void main(String []args){
+              
+	 char A = 65;
+ 		 System.out.println(A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n'
+                                    + A+"-"+(int)A++ +'\n');
+              	}
+}
