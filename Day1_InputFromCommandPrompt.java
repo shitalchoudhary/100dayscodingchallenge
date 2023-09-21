0bb8dfbac591d99711ed6ac41ca6351ada2feb86
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-09-21 19:31:15 +0530

    Add files via upload

---
+++ Day1_InputFromCommandPrompt.java
@@ -0,0 +1,6 @@
+class InputFromCommandPrompt{
+                                                 public static void main(String []args){
+                             
+                                        System.out.println("Hello" +args[0]);
+                                        }
+}
