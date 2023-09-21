46ed4ebbb254b24d1a947aaaf06a0bc871ab92c8
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-09-21 20:07:53 +0530

    Add files via upload

---
+++ Day1_JAVAusigASCII.java
@@ -0,0 +1,11 @@
+class ASCII{
+                      public static void main(String []args){
+                            
+                      char a1=74;
+                      char a2=65;
+                      char a3=86;
+                      char a4=65;
+                   System.out.println(" "+a1+a2+a3+a4);
+                   }
+}
+            
