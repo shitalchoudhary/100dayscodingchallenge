dd31eaf203c1c4abd0a1adb2195441a6066fb8e8
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-09-23 13:53:29 +0530

    Add files via upload

---
+++ Day3_UsingEscaps.java
@@ -0,0 +1,7 @@
+class Escaps
+{
+         public static void main(String []args)
+         {
+             System.out.println(" Esay he code\n Esay he code\n Esay he code\n Esay he code\n Esay he code");
+}
+}
