580788dd1461c35e2cb13e39b83551bb8a5724d2
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-09-23 15:04:31 +0530

    Add files via upload

---
+++ Day3_MultiplyTwoNumbers.java
@@ -0,0 +1,10 @@
+class Multiplication
+{
+              public static void main(String []args)
+              {
+                     int a = 5;
+                     int b = 2;
+                     int c = a*b;
+                      System.out.println(" multiplicatio of c = " +c);
+                 }
+}
