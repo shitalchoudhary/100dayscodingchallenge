a1f78eac20a722076e6ee63b904790d3d51c1a0f
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-09-23 12:08:29 +0530

    Add files via upload

---
+++ Day2_swapping.java
@@ -0,0 +1,15 @@
+class swap{
+                    public static void main(String []args){
+        
+                   int A = 30;
+                   int B = 40;
+                   int C;
+        
+                    C = A;
+                    A = B;
+                    B = C;
+    
+                  System.out.println("A = "+A);
+                   System.out.println("B = "+B);
+                 }
+}
