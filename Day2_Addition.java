a8396cbde65883ea369ed40d4043e3a566fb6e15
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-09-23 11:53:33 +0530

    Add files via upload

---
+++ Day2_Addition.java
@@ -0,0 +1,13 @@
+class Addition{
+                         public static void main(String []args){
+                     
+                         
+                          int A = 10;
+                          int B = 20;
+                          int C = 30;
+                          int D = 40;
+                          int E = 50;
+            
+                     System.out.println("Addition of 5 numbers is = "+(A+B+C+D+E));
+                     }
+}
