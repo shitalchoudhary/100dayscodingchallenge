52af4ea1530695b18f57d6693c5a6723bc9dd7cb
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-09-23 12:32:37 +0530

    Add files via upload

---
+++ Day2_DivisibilityCheck.java
@@ -0,0 +1,19 @@
+class DivisibilityCheck
+{
+              public static void main(String []args)
+                            {
+                             
+                                    int x = 21;
+                            if(x%3==0 && x%7==0)
+                                      {
+                                            
+                                             System.out.println("yes");
+                                        }
+                            else
+
+                                     {        
+                                             System.out.println("No");
+                                      }
+                             }
+
+}
