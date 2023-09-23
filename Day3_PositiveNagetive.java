6bb0cc3705b06758caa3bb3b746e8e92ca22ab1f
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-09-23 14:58:15 +0530

    Add files via upload

---
+++ Day3_PositiveNagetive.java
@@ -0,0 +1,18 @@
+class PositiveNagetive
+{
+       public static void main(String []args)
+       {
+           int a = -11;
+            
+        if(a>0)
+           
+               {
+                     System.out.println(a+"number is Positive");
+                }
+        else if(a<0)
+                  
+                 {
+                     System.out.println(a+"number is Negative");
+                }
+         }
+}
