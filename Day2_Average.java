b27351e0cafa3f03cd5e0a62ee5309b49a24c09e
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-09-23 12:00:47 +0530

    Add files via upload

---
+++ Day2_Average.java
@@ -0,0 +1,12 @@
+class Average{
+                         public static void main(String[]args){
+                        
+                         int a=50;
+                         int b=30;
+                         int c=10;
+                         int d=15;
+                         int e=20;
+                 
+                         System.out.println("Average of 5 numbers is = "+(a+b+c+d+e)/5);
+                        }
+}
