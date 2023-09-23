4063d26a058f8c97fc39637a56a68a8ba30048f1
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-09-23 12:44:24 +0530

    Add files via upload

---
+++ Day2_EvenOdd.java
@@ -0,0 +1,20 @@
+class EvenOdd
+{
+                        public static void main(String []args)
+                        {
+                            
+                              int S = 11;
+                          
+                          if(S%2==0)
+                               {
+                                      System.out.println(S+ " is even number");
+                                }
+                         
+                          else
+                           
+                                 {
+                                       System.out.println(S+ " is odd number");
+                                  }
+
+                          }
+}       
