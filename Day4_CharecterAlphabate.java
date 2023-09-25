650e3433303abf98c13e229c5a6065686ac16247
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-09-25 15:18:13 +0530

    Add files via upload

---
+++ Day4_CharecterAlphabate.java
@@ -0,0 +1,16 @@
+class Alphabate{
+    public static void main(String []args)
+    {
+       char value = '^';
+        if(value>='A' && value<='Z' || value>='a' && value<='z')
+         {
+            System.out.println(value+ " = is Alphabate");
+          }
+      
+         else
+            
+           {
+            System.out.println(value+ " = is not Alphabate");
+          }
+       }
+}
