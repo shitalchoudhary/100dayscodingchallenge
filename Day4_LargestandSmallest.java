738051c5a284a950f45b6d51bf9369d46a5a11ab
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-09-25 16:43:31 +0530

    Add files via upload

---
+++ Day4_LargestandSmallest.java
@@ -0,0 +1,34 @@
+class  largestANDsmallest{
+public static void main(String []args)
+{
+      int a=100;
+      int b=23;
+      int c=30;
+      if(a>b && a>c)
+       {
+            System.out.println(a+"= is Largest");
+        }
+        else if(b>a && b>c)
+        {
+             System.out.println(b+" = is Largest");
+         }
+        else
+        {
+             System.out.println(c+" = is Largest");
+         }
+         if(a<b && a<c)
+        {
+             System.out.println(a+" = is Smallest");
+         }
+         else if(b<a && b<c)
+          {
+             System.out.println(b+" = is Smallest");
+          }
+          else
+          {
+             System.out.println(c+" = is Smallest");
+         }
+        
+}
+}
+         
