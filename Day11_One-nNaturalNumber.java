7670af1cfa49e2f3eb6ec3fdb0f68da39e30ba62
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-10-01 16:58:47 +0530

    Add files via upload

---
+++ Day11_One-nNaturalNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class _1_To_n_NaturalNumber
+{
+      public static void main(String []args)
+{
+    Scanner sc = new Scanner(System.in);
+    System.out.println(" Enter the value = ");
+    int n = sc.nextInt();
+    System.out.println("The Natural 1 to "+n+" is= ");
+    int i;
+      for(i=1; i<=n; i++)
+      {
+           System.out.println(i);
+       }
+}
+}
+     
+     
