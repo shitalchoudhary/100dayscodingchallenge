afeb60babb237f27ec76987a32d4b97b0544e5f6
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-10-01 16:04:41 +0530

    Add files via upload

---
+++ Day10_TableOfFive.java
@@ -0,0 +1,14 @@
+class TableOfFive
+{
+       public static void main(String []args)
+{
+      int i;
+      for(i=5; i<=50; i++)
+      {
+              if(i%5==0)
+              {
+                     System.out.println(i);
+               }
+      }
+}
+}
