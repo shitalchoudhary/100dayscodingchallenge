3fc76d7e9c31b4dc7ecbcf50ebc1e07ca59a3739
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-10-02 15:32:43 +0530

    Add files via upload

---
+++ Day12_aTOzAlphabet.java
@@ -0,0 +1,11 @@
+class aTOzAlphabet
+{
+  public static void main(String []args)
+{
+    char Alphabat;
+    for(Alphabat='a'; Alphabat<='z'; Alphabat++)
+    {
+         System.out.print(" "+Alphabat);
+    }
+}
+}
