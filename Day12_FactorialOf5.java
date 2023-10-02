2ad7a914c14b898b1d5135ff63d6ba67caf85aec
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-10-02 15:47:10 +0530

    Add files via upload

---
+++ Day12_FactorialOf5.java
@@ -0,0 +1,13 @@
+class FactorialOf5
+{
+     public static void main(String []args)
+{
+    int i , fact=1;
+    int num =5;
+    for(i=1; i<=5; i++)
+    {
+        fact=fact*i;
+     }
+        System.out.println("Factorial of 5 is-->  "+fact);
+}
+}
