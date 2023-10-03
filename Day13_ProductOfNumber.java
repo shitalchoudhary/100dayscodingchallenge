08c153878310a3950c8bcb9bb4a96fde58cc6891
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-10-03 10:19:12 +0530

    Add files via upload

---
+++ Day13_ProductOfNumber.java
@@ -0,0 +1,14 @@
+class ProductOfNumber
+{
+    public static void main (String []args)
+{
+       int i,product=1;
+       for(i=1; i<=10; i++) 
+       {
+          System.out.println(i);
+         
+        product= product*i;
+       }
+        System.out.println("Product of 1 to 10 is  = "+product);
+ }
+}
