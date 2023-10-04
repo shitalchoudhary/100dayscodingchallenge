7cfd17934db2a85753ca471f1bbde8f62f59a4ca
shitalchoudhary <145602545+shitalchoudhary@users.noreply.github.com>
2023-10-04 22:39:26 +0530

    Add files via upload

---
+++ Day14_CubeOf_1Ton.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class cubeOf1Ton
+{
+public static void main(String []args)
+{
+    Scanner sc=new Scanner(System.in);
+     System.out.print("Enter number-->");    
+    int n=sc.nextInt();     
+     int i;
+ i=n*n*n;
+System.out.println("Cube of "+n+" is = "+i);
+}
+}
